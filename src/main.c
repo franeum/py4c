9bd5c741385158f0e4739f7f6065b4f2fe2929fd
franeum desk <elettrochuck@gmail.com>
2022-04-13 16:52:28 +0200

    updated

--- src/main.c
+++ src/main.c
@@ -2,13 +2,12 @@
 #include <Python.h>
 #include <stdlib.h>
 
-const char* p = "python3.7";
-
 int main(int argc, char* argv[])
 {
     // wchar_t* program = Py_DecodeLocale("python3", NULL);
     Py_SetPath(Py_DecodeLocale(
-        "/home/neum/anaconda3/lib/python37.zip:/home/neum/anaconda3/lib/python3.7:/home/neum/anaconda3/lib/python3.7/lib-dynload:/home/neum/.local/lib/python3.7/site-packages:/home/neum/anaconda3/lib/python3.7/site-packages", NULL));
+        "/usr/local/lib/python39.zip:/usr/local/lib/python3.9:/usr/local/lib/python3.9/lib-dynload:/home/neum/Documenti/py4c/venv39/lib/python3.9/site-packages", NULL));
+    //"/usr/local/lib/python39.zip:/usr/local/lib/python3.9:/usr/local/lib/python3.9/lib-dynload:/home/neum/Documenti/py4c/venv39/lib/python3.9/site-packages", NULL));
     /*
     if (program == NULL) {
         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
@@ -16,16 +15,24 @@ int main(int argc, char* argv[])
     }
     */
     // Py_SetProgramName((wchar_t*)program); /* optional but recommended */
+    /*
     printf("PATH: %s", Py_EncodeLocale(Py_GetPath(), NULL));
     printf("python: %s\n", Py_EncodeLocale(Py_GetProgramName(), NULL));
     printf("version: %s\n", Py_GetVersion());
+    */
+    Py_SetProgramName(Py_DecodeLocale("python3.9", NULL));
+    printf("PATH: %s", Py_EncodeLocale(Py_GetPath(), NULL));
 
+    printf("version: %s\n", Py_GetVersion());
     Py_Initialize();
     PyRun_SimpleString("from time import time,ctime\n"
                        "print('Today is', ctime(time()))\n");
     PyRun_SimpleString("import sys\n"
                        "print(sys.version)\n");
-    PyRun_SimpleString("print(help('modules'))\n");
+    PyRun_SimpleString("import numpy as np\n"
+                       "print(np.array([1,3,5,7,9]))\n");
+    // PyRun_SimpleString("import requests\n");
+
     if (Py_FinalizeEx() < 0) {
         exit(120);
     }
